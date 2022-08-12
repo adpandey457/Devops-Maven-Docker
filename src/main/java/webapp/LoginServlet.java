d4a21eba1f12074e32901694b1855aa64ef64b0b
adpandey457 <99174126+adpandey457@users.noreply.github.com>
2022-08-12 14:29:14 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Lab Practice</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1> Myself Aditi </h1>");
+		out.println("<h1> I am Aditi </h1>");
 		out.println("</body>");
 		out.println("</html>");
 
